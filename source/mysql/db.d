994feca544414d83ca668e65820b69d4d72eb3c4
Nick Sabalausky <Abscissa>
2013-05-13 21:02:15 -0400

    Use -version idents so users can choose Vibe.d or Phobos sockets without changing their code.

--- source/mysql/db.d
+++ source/mysql/db.d
@@ -13,7 +13,7 @@ class MysqlDB {
       string m_database;
       ushort m_port;
       SvrCapFlags m_capFlags;
-      ConnectionPool!(Connection!mySQLSocketVibeD) m_pool;
+      ConnectionPool!(ConnectionX!mySQLSocketVibeD) m_pool;
    }
 
    this(string host, string user, string password, string database, ushort port = 3306, SvrCapFlags capFlags = defaultClientFlags)
@@ -24,13 +24,13 @@ class MysqlDB {
       m_database = database;
       m_port = port;
       m_capFlags = capFlags;
-      m_pool = new ConnectionPool!(Connection!mySQLSocketVibeD)(&createConnection);
+      m_pool = new ConnectionPool!(ConnectionX!mySQLSocketVibeD)(&createConnection);
    }
 
    auto lockConnection() { return m_pool.lockConnection(); }
 
-   private Connection!mySQLSocketVibeD createConnection()
+   private ConnectionX!mySQLSocketVibeD createConnection()
    {
-      return new Connection!mySQLSocketVibeD(m_host, m_user, m_password, m_database, m_port, m_capFlags);
+      return new ConnectionX!mySQLSocketVibeD(m_host, m_user, m_password, m_database, m_port, m_capFlags);
    }
 }
